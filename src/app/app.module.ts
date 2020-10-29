dcf0372f177779f1e70b32ab5cb78a654f87050a
Hakan Macat <hakan.macat@kocsistem.com.tr>
2020-10-29 14:01:34 +0300

    add guard ü

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component'
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe'
+import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component'
-import { WelcomeComponent } from './home/welcome.component'
+import { ProductDetailComponent } from './products/product-detail.component';
+import { ProductDetailGuard } from './products/product-detail.guard';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,10 @@ import { WelcomeComponent } from './home/welcome.component'
     HttpClientModule,
     RouterModule.forRoot([
       { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', component: ProductDetailComponent },
+      { 
+        path: 'products/:id',
+        canActivate:[ProductDetailGuard],
+        component: ProductDetailComponent },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
